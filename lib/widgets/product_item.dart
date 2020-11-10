bbe50166e0563882b242caab2b7c9bdfa5f3dc9e
Kalam031 <abukalam973@gmail.com>
2020-11-10 17:06:40 +0600

    adding snackbar and putting undo action logic

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -49,13 +49,28 @@ class ProductItem extends StatelessWidget {
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
-              icon: Icon(
-                Icons.shopping_cart,
-                color: Theme.of(context).accentColor,
-              ),
-              onPressed: () {
-                cart.addItem(product.id, product.price, product.title);
-              }),
+            icon: Icon(
+              Icons.shopping_cart,
+              color: Theme.of(context).accentColor,
+            ),
+            onPressed: () {
+              cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    'Added Item to Cart!',
+                  ),
+                  duration: Duration(seconds: 2),
+                  action: SnackBarAction(
+                      label: 'Undo',
+                      onPressed: () {
+                        cart.removeSingleItem(product.id);
+                      }),
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
